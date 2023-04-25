46741243b3f2d656e9b4a1bb1ebf44f3086b95fd
gokul.rajan <gokul.rajan@games24x7.com>
2023-04-25 15:39:50 +0530

    Added Quiet and batch mode to reduce output

--- hooks/SpotlessLint.sh
+++ hooks/SpotlessLint.sh
@@ -2,16 +2,14 @@
 set -eu
 
 if [[ -f "mvnw" ]] && [[ -x "mvnw" ]]; then
-    echo "Going to format files local"
-    ./mvnw spotless:apply
-    echo "INFO: Formatted files local"
+    ./mvnw spotless:apply --batch-mode --quiet
+    echo -ne "\nFiles successfully reformatted.\nPlease run git add and commit once again to commit your code."
     exit 0
 fi
 
 if [[ -x "$(command -v mvn)" ]]; then
-    echo "Going to format files"
-    mvn spotless:apply
-    echo "INFO: Formatted files"
+    mvn spotless:apply --batch-mode --quiet
+    echo -ne "\nFiles successfully reformatted.\nPlease run git add and commit once again to commit your code."
     exit 0
 else
     echo "ERROR: Maven is not installed in your system."
