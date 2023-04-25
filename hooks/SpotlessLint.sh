59d51d75510f73c657c2aba78018392a43bac34b
gokul.rajan <gokul.rajan@games24x7.com>
2023-04-25 15:57:45 +0530

    Added green colored output for easier visibility

--- hooks/SpotlessLint.sh
+++ hooks/SpotlessLint.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 set -eu
+COLOR_GREEN='\033[1;32m'
+COLOR_OFF='\033[0m'
 
 if [[ -f "mvnw" ]] && [[ -x "mvnw" ]]; then
     ./mvnw spotless:apply --batch-mode --quiet
-    echo -ne "\nFiles successfully reformatted.\nPlease run git add and commit once again to commit your code."
+    echo -e "\n${COLOR_GREEN}Files successfully reformatted.\nPlease run git add and commit once again to commit your code.${COLOR_OFF}"
     exit 0
 fi
 
 if [[ -x "$(command -v mvn)" ]]; then
     mvn spotless:apply --batch-mode --quiet
-    echo -ne "\nFiles successfully reformatted.\nPlease run git add and commit once again to commit your code."
+    echo -e "\n${COLOR_GREEN}Files successfully reformatted.\nPlease run git add and commit once again to commit your code.${COLOR_OFF}"
     exit 0
 else
     echo "ERROR: Maven is not installed in your system."
